371ac869d02311befaeba0e0a890f4bdc83e88f5
Romka <info@photori.eu>
2017-09-10 23:24:39 +0300

    DnD HTML5

--- src/dnd.js
+++ src/dnd.js
@@ -4,6 +4,8 @@
  * При нажатии на кнопку должен создаваться div со случайными размерами, цветом и позицией
  * Необходимо предоставить возможность перетаскивать созданные div при помощи drag and drop
  * Запрощено использовать сторонние библиотеки. Разрешено пользоваться только тем, что встроено в браузер
+ * 
+ * https://jsfiddle.net/zbhmz4q0/15/
  */
 
 /**
@@ -26,7 +28,8 @@ function createDiv() {
     var div = document.createElement('div'),
         width = Math.floor(Math.random() * 200 + 50),
         height = Math.floor(Math.random() * 200 + 50);
-
+    div.setAttribute('draggable', true);
+    div.setAttribute('id', (new Date()).getTime())
     div.className = 'draggable-div';
     div.style.position = 'absolute';
     div.style.width = width + 'px';
@@ -43,43 +46,38 @@ function createDiv() {
  *
  * @param {Element} target
  */
-function addListeners(target) {
-    target.addEventListener('dragstart', function(e) {
-        e.preventDefault();
-    })
-    target.addEventListener('mousedown', function(e) {
+ function addListeners(target) {
+    document.addEventListener('dragstart', function(e) {
         if (e.target.className != 'draggable-div') {
 
             return;
         }
-        var coords,
-            shiftX, 
-            shiftY,
-            move,
-            stop,
-            element = e.target;
+        var coords = e.target.getBoundingClientRect(),
+            obj = {
+                shiftX: e.clientX - coords.left + pageXOffset,
+                shiftY: e.clientY - coords.top + pageYOffset,
+                id: e.target.id
+            };
 
-        element.style.zIndex = 9999;
-        coords = e.target.getBoundingClientRect();
-        shiftX = e.pageX - coords.left + pageXOffset;
-        shiftY = e.pageY - coords.top + pageYOffset;
-        move = (event) => {
-            element.style.left = event.pageX - shiftX + 'px';
-            element.style.top = event.pageY - shiftY + 'px';
-        },
-        stop = () => {
-            element.style.zIndex = 'auto';
-            document.removeEventListener('mousemove', move)
-            document.removeEventListener('mouseup', stop)
-        };
-        document.addEventListener('mousemove', move)
-        document.addEventListener('mouseup', stop);
-        
+        e.dataTransfer.setData('text/plain', JSON.stringify(obj));
     });
+    document.addEventListener('dragover', event => {
+        event.preventDefault();
+        event.dataTransfer.dropEffect = "move";
+    })
+    document.addEventListener('drop', (event) => {
+        event.preventDefault();
+        let data = JSON.parse(event.dataTransfer.getData("text"));
+        let element = document.getElementById(data.id)
+        element.style.left = event.clientX - data.shiftX + 'px';
+        element.style.top = event.clientY - data.shiftY + 'px';
+    });
+
 }
-addListeners(homeworkContainer);// вместо обработчика на строке 88
+
 let addDivButton = homeworkContainer.querySelector('#addDiv');
 
+addListeners();
 addDivButton.addEventListener('click', function() {
     // создать новый div
     let div = createDiv();
@@ -87,7 +85,7 @@ addDivButton.addEventListener('click', function() {
     // добавить на страницу
     homeworkContainer.appendChild(div);
     // назначить обработчики событий мыши для реализации d&d
-    // addListeners(div); //<<<< закоментировано, поскольку не вижу смысла в обработчике на каждом div'е
+    //addListeners(div); //<<<< закоментировано, поскольку не вижу смысла в обработчике на каждом div'е
     // можно не назначать обработчики событий каждому div в отдельности, а использовать делегирование
     // или использовать HTML5 D&D - https://www.html5rocks.com/ru/tutorials/dnd/basics/
 });
